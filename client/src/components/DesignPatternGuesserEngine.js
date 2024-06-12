edfff84e6ae7d5677c85094b020ec58e1cbb6e79
Nguyễn Trần Trung Nguyên <trungnguyen123akohshit@gmail.com>
2024-06-12 22:26:06 +0700

    things

---
+++ client/src/components/DesignPatternGuesserEngine.js
@@ -0,0 +1,108 @@
+class DesignPatternGuesserEngine {
+    constructor() {
+        // TODO: these "need tweaking" information need to be in the brain 
+        this.ANSWERS_ENUM = {
+            YES: "Yes", 
+            NO: "No", 
+            DONT_KNOW: "Don't Know", 
+            PROBABLY_YES: "Probably Yes", 
+            PROBABLY_NO: "Probably No"
+        };
+
+        this.answerValueMapping = {
+            [this.ANSWERS_ENUM.YES]: 0.8,
+            [this.ANSWERS_ENUM.NO]: 0.4,
+            [this.ANSWERS_ENUM.DONT_KNOW]: .5,
+            [this.ANSWERS_ENUM.PROBABLY_YES]: .7,
+            [this.ANSWERS_ENUM.PROBABLY_NO]: .7,
+        };
+
+        this.evidenceToQuestionMapping =  {
+            studiedMoreThan5Hours: "Do you studied more than 5 hours a day for this subject ?", 
+            attendedAllClasses: "Do you attented all classes of this subject ?", 
+            participatedInStudyGroups: "Are you in any particular study group ?",
+        }; 
+
+
+
+
+        //this.evKeys = Object.keys(this.evidenceToQuestionMapping);
+        this.questionList = Object.values(this.evidenceToQuestionMapping);
+
+        this.init();
+    }
+
+    init() {
+        this.questionIndex = 0;
+        this.evidence = {};
+        this.done = false;
+    }
+
+    done() {
+        return this.done;
+    }
+
+    nextQuestion() {
+        // TODO: should we block the system if the current question haven't been answered ? 
+        if(this.questionIndex < this.questionList.length) {
+            const answers = Object.values(this.ANSWERS_ENUM);
+            //const question = this.evidenceToQuestionMapping[evKeys[this.questionIndex]];
+            const question = this.questionList[this.questionIndex];
+            this.questionIndex++;
+            return {question: question, possibleAnswers: answers};
+        } else {
+            this.done = true;
+            return -1;
+        }
+    }
+
+    answerCurrentQuestion(answerIndex) {
+        // TODO: turn the answer into evidence value and add it to the evidence object 
+    }
+
+
+    // Function to calculate posterior probability for each hypothesis given evidence
+    _calculatePosterior(hypothesis, evidence) {
+        const prior = hypotheses[hypothesis];
+        let likelihood = 1; // Initialize likelihood as 1
+        for (const ev in evidence) {
+            likelihood *= likelihoods[hypothesis][ev];
+        }
+        const posterior = (prior * likelihood);
+        return posterior;
+    }
+
+    // Function to find the most likely hypothesis given evidence
+    _findMostLikelyHypothesis(evidence) {
+        let maxPosterior = -1;
+        let mostLikelyHypothesis = null;
+
+        // Iterate through hypotheses and calculate posterior probabilities
+        for (const hypothesis in hypotheses) {
+            const posterior = calculatePosterior(hypothesis, evidence);
+            console.log(hypothesis, posterior);
+            if (posterior > maxPosterior) {
+                maxPosterior = posterior;
+                mostLikelyHypothesis = hypothesis;
+            }
+        }
+
+        return mostLikelyHypothesis;
+    }
+
+
+    result() {
+        if(!this.done) return "The show haven't done yet";
+        return this._findMostLikelyHypothesis(this.evidence);
+    }
+}
+
+
+const designPatternGuesser = new DesignPatternGuesser();
+designPatternGuesser.init();
+console.log(designPatternGuesser.nextQuestion());
+console.log(designPatternGuesser.nextQuestion());
+console.log(designPatternGuesser.nextQuestion());
+console.log(designPatternGuesser.nextQuestion());
+console.log(designPatternGuesser.nextQuestion());
+console.log(designPatternGuesser.nextQuestion());
--- client/src/components/test.js
+++ client/src/components/test.js
@@ -1,88 +1,142 @@
-const questions = []; 
-const designPatterns = [];
-const answers = []; 
-
-// TODO: let's do something small first
-// version 1: 
-    // 1 question
-    // 2 answers
-    // 3 patterns
-
-// TODO: 
-// version 2: 
-    // 2 questions
-    // 2 answers
-    // 5 patterns
-
-// TODO: 
-// version 3: 
-    // 5 questions
-    // 5 answers
-    // 5 patterns
+// NOTE: THIS IS HAVE OF THE SYSTEM
+// Define hypotheses and their prior probabilities
+const hypotheses = {
+    likelyToPass: 0.3,
+    moderateChance: 0.5,
+    unlikelyToPass: 0.2
+};
 
 
+// Define likelihoods for each hypothesis and evidence
+const likelihoods = {
+    likelyToPass: {
+        // these are the questions
+        studiedMoreThan5Hours: 0.9, 
+        attendedAllClasses: 0.8, 
+        participatedInStudyGroups: 0.7
+    },
+    moderateChance: {
+        studiedMoreThan5Hours: 0.7,
+        attendedAllClasses: 0.6,
+        participatedInStudyGroups: 0.5
+    },
+    unlikelyToPass: {
+        studiedMoreThan5Hours: 0.4,
+        attendedAllClasses: 0.3,
+        participatedInStudyGroups: 0.2
+    }
+};
 
-// TODO: we need to define 2 steps:
-// 1. update the overallknowledge @Current
-// how the updating works:
-//  use **CONDITIONAL PROBABILITIES** and update beliefs as new information becomes available.
+// Function to calculate posterior probability for each hypothesis given evidence
+function calculatePosterior(hypothesis, evidence) {
+    const prior = hypotheses[hypothesis];
+    let likelihood = 1; // Initialize likelihood as 1
+    for (const ev in evidence) {
+        likelihood *= likelihoods[hypothesis][ev];
+    }
+    const posterior = (prior * likelihood);
+    return posterior;
+}
 
-// 2. determine the next question in the designPatternsLikelyHood
+// Function to find the most likely hypothesis given evidence
+function findMostLikelyHypothesis(evidence) {
+    let maxPosterior = -1;
+    let mostLikelyHypothesis = null;
 
-/*
-const overallknowledge = ;
-    -> the overalll probablity distribution
-const thefinalanswer = ;
-    -> the tip of the distribution, MLE - maximum likelyhood esitmation
+    // Iterate through hypotheses and calculate posterior probabilities
+    for (const hypothesis in hypotheses) {
+        const posterior = calculatePosterior(hypothesis, evidence);
+        console.log(hypothesis, posterior);
+        if (posterior > maxPosterior) {
+            maxPosterior = posterior;
+            mostLikelyHypothesis = hypothesis;
+        }
+    }
 
-the evidence_i value is the value of the chosen answer in each 
+    return mostLikelyHypothesis;
+}
 
-const designPatternsLikelyHood = [ 
-    question A: { // each of these questions can be visualized as a rectangle
-        answer a: {
-            design pattern A:, 
-            design pattern B:, 
-        }, 
-        answer b: {
-            design pattern A:, 
-            design pattern B:, 
-        }, 
-        answer c: {
-            design pattern A:, 
-            design pattern B:, 
-        }, 
-    }, 
+// NOTE: mapping of likelyhood values
+// Each evidence can have 1 of 5 values: 
+    // Yes: .8
+    // No: .4
+    // Don't know: .5
+    // Probably Yes: .7
+    // Probably No:  .7
 
-    question B: { // each of these questions can be visualized as a rectangle
-        answer a: {
-            design pattern A:, 
-            design pattern B:, 
-        }, 
-        answer b: {
-            design pattern A:, 
-            design pattern B:, 
-        }, 
-        answer c: {
-            design pattern A:, 
-            design pattern B:, 
-        }, 
-    }, 
 
-];
-*/
+// TODO: write a ui system to ask questions, the output of the system will be the evidence 
+// Evidence
 
-/* NOTE:
-// Each question have a catersian product of design patterns and possible answers
-    // hypothesis A: design patterns 
-    // evidence B: the answers
-    // what we need to calulate for each question: 
-        // given answer B what's the probablity that the design pattern is A 
-        // -> P(A|B)
-    // what we need to predefined for each question: 
-        // P(B|A)
-        // P(A)
-        // P(B)
-    Considering each question is a rectangle   
-        the columns are the possible answers 
-        the rows are the possible design patterns 
-*/
+
+const getEvidenceFromUser = () => {
+    const ANSWERS_ENUM = {
+        YES: "Yes", 
+        NO: "No", 
+        DONT_KNOW: "Don't Know", 
+        PROBABLY_YES: "Probably Yes", 
+        PROBABLY_NO: "Probably No"
+    };
+
+    const answerValueMapping = {
+        [ANSWERS_ENUM.YES]: 0.8,
+        [ANSWERS_ENUM.NO]: 0.4,
+        [ANSWERS_ENUM.DONT_KNOW]: .5,
+        [ANSWERS_ENUM.PROBABLY_YES]: .7,
+        [ANSWERS_ENUM.PROBABLY_NO]: .7,
+    };
+
+    const evidenceToQuestionMapping =  {
+        studiedMoreThan5Hours: "Do you studied more than 5 hours a day for this subject ?", 
+        attendedAllClasses: "Do you attented all classes of this subject ?", 
+        participatedInStudyGroups: "Are you in any particular study group ?",
+    }; 
+
+    // NOTE: THIS IS THE QUESTION RENDERING PART 
+    // TURN THIS INTO A STATE MACHINE SO THAT IT INDEPENDENT FROM THE UI OR ANY RENDERING
+    // FROM THIS TIL END
+    const evidence = {};
+
+    for(const evkey in evidenceToQuestionMapping) {
+        const question = evidenceToQuestionMapping[evkey];
+        console.log(question);
+
+        let answerIndex = 1;
+        let answerStringList = "";
+        for(const anskey in ANSWERS_ENUM) {
+            const answer = ANSWERS_ENUM[anskey];
+            answerStringList += `${answerIndex}.${answer}\n`;
+            answerIndex++;
+        }
+
+        console.log(answerStringList);
+
+        // TODO: wait for user to input the answer 
+        // ...@Later
+
+        const userAnswer = ANSWERS_ENUM.YES;
+
+        evidence[evkey] = answerValueMapping[userAnswer];
+    }
+
+    return evidence; 
+}
+
+const CLICollectingEvidence = () => {
+}
+
+const evidence = getEvidenceFromUser(); // NOTE: this part should be rewrite into UI 
+console.log(evidence);
+
+
+// Find the most likely hypothesis given the evidence
+const mostLikelyHypothesis = findMostLikelyHypothesis(evidence);
+console.log("Most likely hypothesis given evidence:", mostLikelyHypothesis);
+
+
+
+
+
+
+// TODO: IMPLEMENT THE INFORMATION ENTROPY SYSTEM 
+// TO NARROW DOWN THE AMOUNT OF QUESTIONS FOR USER TO ANSWERS
